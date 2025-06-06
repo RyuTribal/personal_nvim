5997b489f0685a4a8acb3bda2492bed1cae7ec65
Ivan Sedelkin <ivan.sedelkin9@gmail.com>
2025-06-06 04:34:06 +0200

    Changed back the swedish bindings to english ones

--- init.lua
+++ init.lua
@@ -191,7 +191,6 @@ vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower win
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Personal keybindings
-vim.keymap.set('o', '¤', '$', { noremap = true, silent = true }) -- Swedish keyboards have an awkward binding to $
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
